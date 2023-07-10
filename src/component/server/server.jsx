323fba34c1f728d3956fb8c3dd9f5745efbe896d
Imrumi-1111 <upasanachakraboty18@gmail.com>
2023-07-10 14:05:02 +0530

    crypto done

---
+++ src/component/server/server.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import styles from './server.module.css'
+
+export default function Api() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const response = await fetch('https://api.coincap.io/v2/assets/bitcoin/markets');
+        const jsonData = await response.json();
+        setCoins(jsonData.data);
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
+    };
+
+    fetchCoins();
+  }, []);
+
+  return (
+    <div className={styles.Api}>
+      <h1>Top 100 Cryptocurrency Coins</h1>
+      <div className={styles.list}>
+        <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            <div className={styles.coins}><strong>{coin.name}</strong> ({coin.symbol}): ${coin.priceUsd}</div>
+          </li>
+        ))}
+      </ul>
+      </div>
+    </div>
+  );
+}
+
+
